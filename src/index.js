976c16b3434da0d9d7e2a262f4ea705d72b68e85
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-10-10 10:52:54 +0300

    update 01

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
+import { refs } from './js/refs';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+function onInputCountriesSearch(evt) {
+  evt.preventDefault();
+}
+
+refs.inputRef.addEventListener(
+  'input',
+  debounce(onInputCountriesSearch, DEBOUNCE_DELAY)
+);
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  inputRef: document.querySelector('#search-box'),
+  countryListRef: document.querySelector('.country-list'),
+  countryInfoRef: document.querySelector('.country-info'),
+};
